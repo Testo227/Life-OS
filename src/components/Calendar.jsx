9ae83c795bbe44fdd57d806bba732a15f264b66e
Testo227 <none>
2026-01-11 01:36:10 +0100

    Kalender ist sauber

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,81 +1,115 @@
-import { useState } from "react";
-import 'boxicons'
+import { useState, useMemo, useRef, useEffect } from "react";
+
+const daysOfWeek = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
 
 const Calendar = () => {
+    const today = new Date();
+    
+    
+    const [selectedMonth, setSelectedMonth] = useState(today.getMonth());
+    const [selectedYear, setSelectedYear] = useState(today.getFullYear())
+    
 
-    const daysOfWeek = ["So","Mo", "Di", "Mi", "Do", "Fr", "Sa", ]
-    const monthsOfYear = ["Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", 'Dezember']
-    const currentDate = new Date()
+    const firstDayOfMonth = new Date(selectedYear, selectedMonth, 1).getDay()
+    const LastDayOfMonth = new Date(selectedYear, selectedMonth+1 , 0).getDay()
+    const daysInMonth = new Date(selectedYear, selectedMonth + 1, 0).getDate()
+    const lastDayOfPrevMonth = new Date(selectedYear, selectedMonth, 0).getDate()
 
-    const [currentMonth, setCurrentMonth] = useState(currentDate.getMonth())
-    const [currentYear, setCurrenYear] = useState(currentDate.getFullYear())
+    
 
-    const daysInMonth = new Date(currentYear, currentMonth + 1, 0).getDate()
-    const firstDayOfMonth = new Date(currentYear, currentMonth, 1).getDay()
 
-    const prevMonth = () => {
-        setCurrentMonth((prevMonth) => (prevMonth === 0 ? 11 : prevMonth-1 ))
-        setCurrenYear((prevYear) => (currentMonth === 0 ? prevYear-1 : prevYear))
+    const monthOptions = monthNames.map((month, index) => ({
+        name: month,
+        value: index
+    }))
+
+    
+    console.log([...Array(firstDayOfMonth).keys()])
+    
+
+    const handleTodayChange = () => {
+        setSelectedMonth(today.getMonth())
+        setSelectedYear(today.getFullYear())
+    }
+    
+    const handleBeginCal = () => {
+        const ZeroMonday = (firstDayOfMonth + 6) % 7
+        const days = []
+        
+        for (let i = ZeroMonday -1; i >=0; i--){
+            days.push(lastDayOfPrevMonth - i)
+        }
+        return days 
     }
 
-    const nextMonth = () => {
-        setCurrentMonth((prevMonth) => (prevMonth === 11 ? 0 : prevMonth+1 ))
-        setCurrenYear((prevYear) => (currentMonth === 11 ? prevYear+1 : prevYear))
+    const handleEndCal = () => {
+        const ZeroMonday = (LastDayOfMonth + 6) % 7
+        const numNextDays = 6 - ZeroMonday
+        const days = []
+
+        for (let i = 1; i <= numNextDays; i++){
+            days.push(i)
+        }
+        return days
     }
+    
+    const [daysOfPrevMonthYear, setDaysOfPrevMonthYear] = useState(() => handleBeginCal())
+    const [daysOfFolMonthYear, setDaysOfFolMonthYear] = useState(() => handleEndCal())
+
+    useEffect(() => {
+        // wird automatisch aufgerufen, wenn selectedMonth sich ändert
+        setDaysOfPrevMonthYear(handleBeginCal())
+        }, [selectedMonth, selectedYear])
+
+    useEffect(() => {
+        // wird automatisch aufgerufen, wenn selectedMonth sich ändert
+        setDaysOfFolMonthYear(handleEndCal())
+        }, [selectedMonth, selectedYear])
+   
+    
+
 
 
     return ( 
-        <div className="m-0 p-0 box-border outline font-sans w-full h-screen bg-[#2c3542]">
-            <div className="w-6/10 bg-[#le242d] p-3">
-                <h1 className="">Kalender</h1>
-                <div className="">
-                    <h2 className="">{monthsOfYear[currentMonth]}</h2>
-                    <h2 className="">{currentYear}</h2>
-                    <div className="flex">
-                        <p onClick={prevMonth}>⬅️</p>
-                        <p onClick={nextMonth}>➡️</p>
-                    </div>
+        <div>
+            <div className="flex gap-2">
+                <select  value={selectedMonth} onChange={(e) => setSelectedMonth(Number(e.target.value))}>
+                    {monthOptions.map( (pair) =>
+                        <option key={pair.value} value={pair.value}>{pair.name}</option>
+                    )}
+                </select>
+                <div className="flex">
+                    <div onClick={() => setSelectedYear(selectedYear - 1)} className="hover:cursor-pointer">⬅️</div>
+                    <div>{selectedYear}</div>
+                    <div onClick={() => setSelectedYear(selectedYear + 1)} className="hover:cursor-pointer">➡️</div>
                 </div>
+                <button onClick={() => handleTodayChange()} className="hover:cursor-pointer">Today</button>
+                <button className="hover:cursor-pointer">+ Neuer Eintrag</button>
+            </div>
+            <div className="flex flex-col">
                 <div className="grid grid-cols-7">
                     {
-                        daysOfWeek.map(day =>
-                            <span key={day}>{day}</span>
+                        daysOfWeek.map((day) =>
+                            <div>{day}</div>
                         )
                     }
                 </div>
                 <div className="grid grid-cols-7">
                     {
-                        [...Array(firstDayOfMonth).keys()].map((_,i) => <span key={`empty-${i}`}></span>)
+                        daysOfPrevMonthYear.map((day) => <div key={day}>{day}</div>)
                     }
                     {
-                        [...Array(daysInMonth).keys()].map((day) => <span key={day+1}>{day+1}</span>)
+                        [...Array(daysInMonth).keys()].map((day) => <div key={day+1}>{day+1}</div>)
                     }
-                </div>
-            </div>
-            <div className="">
-                <div>
-                    <div>Zeit</div>
-                    <input type="number" name="hours" min={0} max={24}></input>
-                    <input type="number" name="minute" min={0} max={60}></input>
-                </div>
-                <textarea placeholder="..."></textarea>
-                <button className="">Hinzufuegen</button>
-                <button>Schliessen</button>
-                <div className="">
-                    <div>
-                        <div>10.01.2026</div>
-                        <div>09:30</div>
-                    </div>
-                    <div>Meeting with Joe</div>
-                    <div>
-                        <button>Edit</button>
-                        <button>Delete</button>
-                    </div>
+                    {
+                        daysOfFolMonthYear.map((day) => <div key={day}>{day}</div>)
+                    }
                 </div>
             </div>
         </div>
      );
 }
  
-export default Calendar;
+export default Calendar ;
