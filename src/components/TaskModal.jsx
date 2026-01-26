66fcc66099afdf4726e58531552736d6cd5b3dfa
Testo227 <none>
2026-01-26 20:43:39 +0100

    Am Kalender gearbeitet

--- src/components/TaskModal.jsx
+++ src/components/TaskModal.jsx
@@ -7,13 +7,20 @@ const TaskModal = ({tasks, setTasks, setShowAddTask}) => {
     const [date, setDate] = useState("");
     const [time, setTime] = useState("");
     const [description, setDescription] = useState("");
-    const [recurrence, setRecurrence] = useState({type:"", days: [""]})
-    const [reccure, setReccure] = useState(false)
+    const [recurrence, setRecurrence] = useState({type:"No", days: [""]})
+    const [individual, setIndividual] = useState(false)
+
+
+    const handleSetReccure = (type) => {
+        type === "Individual" ? setIndividual(true) : setIndividual(false)
+        
+    }
+
 
     // zum Testen 
     useEffect(
-        () => console.log(title, date, time, description, recurrence)
-    ),[title, date, time, description, recurrence]
+        () => console.log(title, date, time, description, recurrence, individual)
+    ),[title, date, time, description, recurrence, individual]
 
     return ( 
         
@@ -71,15 +78,42 @@ const TaskModal = ({tasks, setTasks, setShowAddTask}) => {
             </div>
 
             {/* Wiederholen / Recurrence (UI nur) */}
-            <div className="flex flex-row gap-2">
+            <div className="flex flex-col gap-2">
             <label className="text-slate-200 mb-1 font-medium">Wiederholen?</label>
-            <input onChange={(e) => setReccure(e.target.checked)} type="checkbox"></input>
-            {reccure ? 
-                <div>
-                    hi
+            <select className="text-slate-200 mb-1 font-medium bg-black/50" onChange={(e) => handleSetReccure(e.target.value)}>
+                <option value="No">No</option>
+                <option value="Daily">Daily</option>
+                <option value="On weekdays">On weekdays</option>
+                <option value="On weekends">On weekends</option>
+                <option value="Weekly">Weekly</option>
+                <option value="Every 2 weeks">Every 2 weeks</option>
+                <option value="Monthly">Monthly</option>
+                <option value="Every 3 months">Every 3 months</option>
+                <option value="Every 6 months">Every 6 months</option>
+                <option value="Yearly">Yearly</option>
+                <option value="Individual">Individual</option>
+            </select>
+            {
+                individual ? 
+                <>
+                <div className="flex flex-row gap-2">
+                <label className="text-slate-200 mb-1 font-small">Frequency:</label>
+                <select className="text-slate-200 mb-1 font-medium bg-black/50">
+                    <option value="Daily">Daily</option>
+                    <option value="Weekly">Weekly</option>
+                    <option value="Monthly">Monthly</option>
+                    <option value="Yearly">Yearly</option>
+                </select>
                 </div>
 
-            : <></>}
+                <div>
+                <label className="text-slate-200 mb-1 font-small">Every:</label>
+                <input className="text-slate-200 mb-1 font-medium bg-black/50" type="number" max="999" min="1"></input>
+                </div>
+                </>
+                : 
+                <></>
+            }
             </div>
 
             {/* Buttons */}
